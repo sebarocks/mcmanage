41aeb3631b8036580820890766e7e4e51d1370d4
Sebarocks <sebastian_orrego_a@outlook.com>
2024-07-09 19:30:40 -0400

    json pydantic validation

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import ipaddress
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dns_updater import updateRecord
 
 class MySettings(BaseSettings):
     model_config = SettingsConfigDict(env_file='.env',extra='allow')
+
+class UpdateIP(BaseModel):
+    nueva_ip: str
+    clave: str
 
 settings = MySettings()
 app = FastAPI()
@@ -23,15 +28,16 @@ def get_ip():
     return read_file()
 
 @app.post("/IP/update")
-def set_ip(nueva_ip: str, clave : str):
+def set_ip(req : UpdateIP):
+    print(req)
     try:
-        ipaddress.ip_address(nueva_ip)
+        ipaddress.ip_address(req.nueva_ip)
     except ValueError:
         raise HTTPException(status_code=400, detail="Formato de IP inválido")
 
-    if clave == settings.master_key:
-        write_file(nueva_ip)
-        updateRecord(nueva_ip, settings.linode_token)
-        return {"es_valido": True, "nueva_ip": nueva_ip}
+    if req.clave == settings.master_key:
+        write_file(req.nueva_ip)
+        updateRecord(req.nueva_ip, settings.linode_token)
+        return {"es_valido": True, "nueva_ip": req.nueva_ip}
     else:
         raise HTTPException(status_code=401, detail="Clave inválida")
