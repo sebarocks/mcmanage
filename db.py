fa655bbcbb8fa3d614ca467744bd03dc5f8ef677
Sebarocks <sebastian_orrego_a@outlook.com>
2024-07-23 21:09:21 -0400

    improved DB added authorized starters

--- db.py
+++ db.py
@@ -1,8 +1,30 @@
+import json
+
+IP_DB = "ip.txt"
+LASTACTIVE_DB = "last.json"
+
 def read_ip():
-    with open("ip.txt", "r") as archivo:
+    with open(IP_DB, "r") as archivo:
         contenido = archivo.read()
     return contenido
 
 def write_ip(ipstr):
-    with open("ip.txt", "w") as archivo:
+    with open(IP_DB, "w") as archivo:
         contenido = archivo.write(ipstr)
+
+def read_lastactive():
+    with open(LASTACTIVE_DB, 'r') as f:
+        return json.load(f)
+
+def write_lastactive(data):
+    with open(LASTACTIVE_DB, 'w') as f:
+        json.dump(data, f, indent=4)
+
+authorized_starters = [
+    'Seba',
+    'Marfull',
+    'Carlos',
+    'Mike',
+    'Benja',
+    'Stay'
+]
