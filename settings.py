6ce2706979a9a3504831769f023e445b49fd57ec
Sebarocks <sebastian_orrego_a@outlook.com>
2024-08-05 13:40:26 -0400

    fix: .env bool variables

--- settings.py
+++ settings.py
@@ -1,6 +1,10 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class MySettings(BaseSettings):
+    
+    pb_enabled : bool
+    activity_check_enabled : bool
+
     model_config = SettingsConfigDict(env_file='.env',extra='allow')
 
 
