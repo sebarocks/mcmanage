fc87c0c9108d6c6e9b8e74a732c654a3be1e2d8a
Sebarocks <sebastian_orrego_a@outlook.com>
2024-08-08 14:50:20 -0400

    removed env validation

--- settings.py
+++ settings.py
@@ -1,9 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class MySettings(BaseSettings):
-    
-    pb_enabled : bool
-    activity_check_enabled : bool
 
     model_config = SettingsConfigDict(env_file='.env',extra='allow')
 
